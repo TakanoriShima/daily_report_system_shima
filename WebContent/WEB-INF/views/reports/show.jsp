2c8dabc91d6c60e1bea7c4f8c1504336135a8f8a
Takanori Shima <quark2galaxy@gmail.com>
2020-11-10 01:09:05 +0900

    favorite and unfavorite done

--- WebContent/WEB-INF/views/reports/show.jsp
+++ WebContent/WEB-INF/views/reports/show.jsp
@@ -1,8 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <c:import url="/WEB-INF/views/layout/app.jsp">
     <c:param name="content">
+        <c:if test="${flush != null}">
+            <div id="flush_success">
+                <c:out value="${flush}"></c:out>
+            </div>
+        </c:if>
         <c:choose>
             <c:when test="${report != null}">
                 <h2>日報　詳細ページ</h2>
@@ -47,8 +53,40 @@
                                 <fmt:formatDate value="${report.updated_at}" pattern="yyyy-MM-dd HH:mm:ss" />
                             </td>
                         </tr>
+                        <tr>
+                            <th>いいねをしてくれた従業員</th>
+                            <td>
+                                ${fn:length(favoritedEmployeeList)}名
+                                <ul>
+                                	<c:forEach var="employee" items="${favoritedEmployeeList}">
+                                		<li><c:out value="${employee.name}"/></li>
+                                	</c:forEach>
+                                </ul>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
+                <br/><br/>
+
+
+				<c:choose>
+					<c:when test="${favoritesOfAttensionCount == 0}">
+		                <form action="${pageContext.request.contextPath}/favorites/create" method="POST">
+							<input type="hidden" name="report_id" value="<c:out value='${report.id}'/>">
+							<input type="hidden" name="_token" value="${_token}" />
+							<button type="submit">いいね</button>
+		                </form>
+					</c:when>
+					<c:otherwise>
+						<form action="${pageContext.request.contextPath}/favorites/destroy" method="POST">
+							<input type="hidden" name="report_id" value="<c:out value='${report.id}'/>">
+							<input type="hidden" name="_token" value="${_token}" />
+							<button type="submit">いいね解除</button>
+		                </form>
+					</c:otherwise>
+				</c:choose>
+
+				<br/><br/>
 
                 <c:if test="${sessionScope.login_employee.id == report.employee.id}">
                     <p><a href="<c:url value="/reports/edit?id=${report.id}" />">この日報を編集する</a></p>
--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -1,6 +1,7 @@
 package controllers.reports;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import models.Favorite;
 import models.Report;
 import utils.DBUtil;
 
@@ -18,31 +21,45 @@ import utils.DBUtil;
  */
 @WebServlet("/reports/show")
 public class ReportsShowServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReportsShowServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ReportsShowServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
 
-        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+		Employee e = (Employee) request.getSession().getAttribute("login_employee");
+		Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+		List<Favorite> favoritesOfAttension = em.createNamedQuery("getFavoritesOfAttension", Favorite.class)
+				.setParameter("employee", e).setParameter("report", r).getResultList();
 
-        em.close();
+		em.close();
 
-        request.setAttribute("report", r);
-        request.setAttribute("_token", request.getSession().getId());
+		List<Employee> favoritedEmployeeList = r.getFavoritedEmployees();
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
-        rd.forward(request, response);
-    }
+		// System.out.println("いいねしてくれている従業員リスト");
+		// for(Employee emp : list){
+		// System.out.println("従業員名" + emp.getName());
+		// }
+
+		request.setAttribute("report", r);
+		request.setAttribute("favoritesOfAttensionCount", favoritesOfAttension.size());
+		request.setAttribute("_token", request.getSession().getId());
+		request.setAttribute("favoritedEmployeeList", favoritedEmployeeList);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+		rd.forward(request, response);
+	}
 
 }
